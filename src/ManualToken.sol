4e1d0450a101c69debdff23d078d8b901b1cbcb1
ThomasHeim11 <wearedreamin@gmail.com>
2023-06-21 13:40:12 +0200

    update contracts✅

--- src/ManualToken.sol
+++ src/ManualToken.sol
@@ -3,6 +3,7 @@ pragma solidity 0.8.19;
 
 contract ManualToken {
     mapping(address => uint256) private s_balances;
+
     function name() public pure returns (string memory) {
         return "Manual Token";
     }
@@ -17,5 +18,12 @@ contract ManualToken {
 
     function balanceOf(address _owner) public view returns (uint256) {
         return s_balances[_owner];
+    }
+
+    function transfer(address _to, uint256 _amount) public {
+        uint256 previousBalance = balanceOf(msg.sender) + balanceOf(_to);
+        s_balances[msg.sender] -= _amount;
+        s_balances[_to] += _amount;
+        require(balanceOf(msg.sender) + balanceOf(_to) == previousBalance);
     }
 }
