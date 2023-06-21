f8700a83f3c9c46984b8339bc9ca0f64011a8bef
ThomasHeim11 <wearedreamin@gmail.com>
2023-06-21 18:12:43 +0200

    update contracts✅

--- src/OurToken.sol
+++ src/OurToken.sol
@@ -1,4 +1,4 @@
-
+// contracts/OurToken.sol
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.19;
 
--- test/OurTokenTest.t.sol
+++ test/OurTokenTest.t.sol
@@ -1,44 +1,59 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.19;
 
-import {Test} from "forge-std/Test.sol";
+pragma solidity ^0.8.19;
+
 import {DeployOurToken} from "../script/DeployOurToken.s.sol";
 import {OurToken} from "../src/OurToken.sol";
+import {Test, console} from "forge-std/Test.sol";
+import {StdCheats} from "forge-std/StdCheats.sol";
 
-contract ourTokenTest is Test {
+interface MintableToken {
+    function mint(address, uint256) external;
+}
+
+contract OurTokenTest is StdCheats, Test {
+    uint256 BOB_STARTING_AMOUNT = 100 ether;
+
     OurToken public ourToken;
     DeployOurToken public deployer;
-
-    address bob = makeAddr("Bob");
-    address alice = makeAddr("alice");
-
-    uint256 public constant STARTING_BALANCE = 1000 ether;
+    address public deployerAddress;
+    address bob;
+    address alice;
 
     function setUp() public {
         deployer = new DeployOurToken();
         ourToken = deployer.run();
 
-        vm.prank(address(msg.sender));
-        ourToken.transfer(bob, STARTING_BALANCE);
+        bob = makeAddr("bob");
+        alice = makeAddr("alice");
+
+        deployerAddress = vm.addr(deployer.deployerKey());
+        vm.prank(deployerAddress);
+        ourToken.transfer(bob, BOB_STARTING_AMOUNT);
     }
 
-    function testBobBalance() public {
-        assertEq(ourToken.balanceOf(bob), STARTING_BALANCE);
+    function testInitialSupply() public {
+        assertEq(ourToken.totalSupply(), deployer.INITIAL_SUPPLY());
     }
 
-    function testAllowancesWorks() public {
+    function testUsersCantMint() public {
+        vm.expectRevert();
+        MintableToken(address(ourToken)).mint(address(this), 1);
+    }
+
+    function testAllowances() public {
         uint256 initialAllowance = 1000;
 
-        // Bob allows Alice to spend tokens on her behalf
+        // Alice approves Bob to spend tokens on her behalf
         vm.prank(bob);
         ourToken.approve(alice, initialAllowance);
-
         uint256 transferAmount = 500;
 
         vm.prank(alice);
         ourToken.transferFrom(bob, alice, transferAmount);
-
         assertEq(ourToken.balanceOf(alice), transferAmount);
-        assertEq(ourToken.balanceOf(bob), STARTING_BALANCE - transferAmount);
+        assertEq(ourToken.balanceOf(bob), BOB_STARTING_AMOUNT - transferAmount);
     }
+
+    // can you get the coverage up?
 }
